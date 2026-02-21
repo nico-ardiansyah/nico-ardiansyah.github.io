70a6bde9a434e16b3052541566d84df56d7e1769
nico-ardiansyah <nic0.ardiansy4h@gmail.com>
2026-02-21 17:10:14 +0700

    update

--- Portofolio/src/components/About.jsx
+++ Portofolio/src/components/About.jsx
@@ -1,14 +1,14 @@
 import "../../src/index.css";
 import foto from "../assets/foto2.png";
 
-const About = () => { 
+const About = () => {
     return (
         <>
             <section className="w-screen h-screen bg-transparent z-5 absolute flex flex-col md:flex-row mt-15 font-poppins md:justify-center md:items-center animate-fade-out">
                 <div className="w-screen h-1/3 flex items-center justify-center relative md:w-1/3">
-                    <div className="w-50 h-50 bg-myGreen rounded-full shadow-[0px_0px_50px] shadow-myGreen md:shadow-none blur-md animate-blob md:w-90 md:h-90"></div>
+                    <div className="w-50 h-50 bg-myGreen rounded-full shadow-[0px_0px_50px] shadow-myGreen md:blur-md animate-blob md:w-90 md:h-90"></div>
                     <div className="w-50 h-50 absolute border border-white rounded-full animate-blob md:w-80 md:h-80">
-                        <img src={ foto } alt="" className="object-cover object-top w-full h-full rounded-full"/>
+                        <img src={foto} alt="" className="object-cover object-top w-full h-full rounded-full" />
                     </div>
                 </div>
                 <div className="flex flex-col md:w-1/2 md:gap-5">
